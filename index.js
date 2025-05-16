d055082bdf64623b188c3f534a5d3006d51c0889
MwangiTess <teresiamwangi26@gmail.com>
2025-05-16 15:50:27 +0300

    wrote the computer logic
    The logic return rocks, paper, scissors.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Hello World");
+/*
+    We are creating the game of rock paper scissors that is to be played in the console.
+    A computer vs a human, they play for 5 rounds.
+*/
+
+// First logic to get the computer choice, that randomly generates and returns  either rock, paper, scissor.
+
+function getComputerChoice() {
+    const play = Math.floor(Math.random() * 3);
+    //  it should return rock, paper, scissors.
+
+    if (play === 0) {
+        return "rock";
+    } else if (play === 1) {
+        return "paper";
+    } else 
+    return "scissors"
+}
+
+console.log(getComputerChoice());
